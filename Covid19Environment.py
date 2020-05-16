3dc8040da64e4a5ca1a7940a1f16046dfef3e0f7
marianylund <marianylund@pm.me>
2020-05-16 12:31:59 +0200

    Strange

--- Covid19Environment.py
+++ Covid19Environment.py
@@ -1,17 +1,133 @@
-from MovableObj import GraphicMovableObj
+from MovableObj import GraphicMovableObj, MovableObj
 import numpy as np
 from random import randint
+import random
 from graphics import *
 import math
 import traceback
 import sys
 import enum 
 
+class States(enum.Enum): 
+    NOTHING = 0
+    HOUSE = 1
+    SOMEONE = 2
+    KIWI = 3
+    BORDER = 4
+
+class Actions(enum.Enum): 
+    LEFT = 0
+    RIGHT = 1
+    FORWARD = 2
+
+class Covid19Environment():
+    
+    have_been_to_kiwi = False
+    total_actions = 0
+    random_actions = 0
+
+    def __init__(self, q_table, epsilon, world_size = (5, 5), learning_rate = 0.7, discount_rate = 0.618):
+        self.learning_rate = learning_rate
+        self.discount_rate = discount_rate
+        self.epsilon = epsilon
+        
+        self.world_size = world_size
+        self.environment_table = np.zeros(world_size, dtype = int)
+        self.environment_table[0][0] = States.HOUSE.value
+        self.environment_table[world_size[0]-1][world_size[1]-1] = States.KIWI.value
+        self.q_table = q_table
+        start_pos = [math.floor(world_size[0]/2), math.floor(world_size[1]/2)]
+        self.survivor = MovableObj(start_pos)
+        #print("vision: ", self.survivor.get_vision_pos())
+        #print("direction: ", self.survivor.states[survivor.currentState]) 
+
+    def is_within_bounds(self, pos):
+        return pos[0] < self.world_size[0] and pos[1] < self.world_size[1] and pos[0] >= 0 and pos[1] >= 0
+
+    def get_stand_state(self, stand_pos):
+        if not self.is_within_bounds(stand_pos):
+            return 4
+        return self.environment_table[stand_pos[0]][stand_pos[1]]
+
+    def get_see_state(self, see_pos):
+        if not self.is_within_bounds(see_pos):
+            return 4
+        return self.environment_table[see_pos[0]][see_pos[1]]
+
+    def get_state(self, stand_pos, see_pos):
+        return self.get_stand_state(stand_pos), self.get_see_state(see_pos)
+
+    def get_possible_actions_from_state(self, state):
+        possible_actions = []
+        for i in range(3):
+            if(self.q_table[state[0]][state[1]][i] != -math.inf):
+                possible_actions.append(i)
+        return possible_actions
+
+    def get_max_value_from_state(self, state):
+        return max(self.q_table[state[0]][state[1]])
+
+    def calculate_reward(self, state):
+        if state[0] == States.NOTHING.value:
+            return -1, False
+        elif state[0] == States.SOMEONE.value:
+            return -10, False
+        elif state[0] == States.KIWI.value:
+            if self.have_been_to_kiwi:
+                return 0, False
+            else:
+                self.have_been_to_kiwi = True
+                return 50, False
+        elif state[0] == States.HOUSE.value:
+            if self.have_been_to_kiwi:
+                print("Wooow! House reached after Kiwi!")
+                return 200, True
+            else:
+                print("House reached")
+                return 30, True
+            
+            return 10, True
+        elif state[0] == States.BORDER.value:
+            print("Out of the border.")
+            return -1000, True
+        else:
+            print("Something is wrong: ", state)
+
+    def step(self):
+        current_state = self.get_state(self.survivor.get_body_pos(), self.survivor.get_vision_pos())
+        #print("current_state: ", current_state)
+        possible_actions = self.get_possible_actions_from_state(current_state)
+        #print("possible_actions: ", possible_actions)
+
+        exp_exp_tradeoff = random.uniform(0,1)
+        if exp_exp_tradeoff > self.epsilon:
+            action_to_choose = np.argmax(self.q_table[current_state[0],current_state[1],:])
+        else:
+            action_to_choose = random.choice(possible_actions)
+            self.random_actions += 1
+
+        new_state = self.get_state(self.survivor.get_body_pos(), self.survivor.get_vision_pos())
+        #print("new_pos: ", survivor.get_body_pos(), survivor.get_vision_pos())
+        #print("new_state: ", States(new_state[0]), States(new_state[1]))
+
+        highest_Q_value = self.get_max_value_from_state(new_state)
+        current_Q_value = self.q_table[current_state[0]][current_state[1]][action_to_choose]
+        reward, done = self.calculate_reward(new_state)
+
+        # New Q value =    Current Q value +    lr * [Reward + discount_rate * (highest Q value between possible actions from the new state s’ ) — Current Q value ]
+        new_Q_value = current_Q_value + self.learning_rate * (reward + self.discount_rate * highest_Q_value - current_Q_value)
+
+        self.q_table[current_state[0]][current_state[1]][action_to_choose] = new_Q_value
+        self.total_actions += 1
+        return done, reward
+        #print("new_Q_value: ", new_Q_value)
+
+    
 class GraphicCovid19Environment():
     reward = 0
-    visited_kiwi = False
+    have_been_to_kiwi = False
 
-    def __init__(self):
+    def __init__(self, q_table_choice = False, q_table = None):
         try:
             self.win = GraphWin('Floor', 500, 500)
             self.win.setCoords(0.0, 0.0, 10.0, 10.0)
@@ -21,6 +137,14 @@ class GraphicCovid19Environment():
             self.add_house()
             self.add_kiwi()
             self.add_survivor()
+            
+            self.world_size = (10, 10)
+            self.environment_table = np.zeros(self.world_size, dtype = int)
+            self.environment_table[0][0] = States.HOUSE.value
+            self.environment_table[self.world_size[0]-1][self.world_size[1]-1] = States.KIWI.value
+            
+            self.q_table = q_table
+            self.q_table_choice = q_table_choice
             
             self.listen_for_keys()   
             self.win.close()
@@ -35,20 +159,40 @@ class GraphicCovid19Environment():
             k = self.win.checkKey()
 
             if k == 'Left':
-                self.do_action(0)
-                self.calculate_rewards(self.get_state())
+                self.do_action(Actions.LEFT.value)
             elif k == 'Right':
-                self.do_action(1)
-                self.calculate_rewards(self.get_state())
+                self.do_action(Actions.RIGHT.value)
             elif k == 'Up':
-                self.do_action(2)
-                self.calculate_rewards(self.get_state())
+                self.do_action(Actions.FORWARD.value)
             elif k == 'Down':
-                self.do_action()
-                self.calculate_rewards(self.get_state())
+                if self.q_table_choice:
+                    self.do_action_from_q_table()
+                else:
+                    self.do_action()
             elif k == 'period':
                 break
-                  
+    
+    def do_action_from_q_table(self):
+        current_state = self.get_state(self.survivor.get_body_pos(), self.survivor.get_vision_pos())
+        action_to_do = np.argmax(self.q_table[current_state[0],current_state[1],:])
+        print(self.q_table[current_state[0],current_state[1],:])
+        self.do_action(action_to_do)
+        
+    def is_within_bounds(self, pos):
+        return pos[0] < self.world_size[0] and pos[1] < self.world_size[1] and pos[0] >= 0 and pos[1] >= 0
+    
+    def get_stand_state(self, stand_pos):
+        if not self.is_within_bounds(stand_pos):
+            return 4
+        return self.environment_table[stand_pos[0]][stand_pos[1]]
+
+    def get_see_state(self, see_pos):
+        if not self.is_within_bounds(see_pos):
+            return 4
+        return self.environment_table[see_pos[0]][see_pos[1]]
+
+    def get_state(self, stand_pos, see_pos):
+        return self.get_stand_state(stand_pos), self.get_see_state(see_pos)
         
     def draw_grid(self):
         print("PlottingGrid")
@@ -83,56 +227,45 @@ class GraphicCovid19Environment():
     def reset_environment(self):
         self.survivor.restart()
     
-    def get_state(self):
-        state_body_vis = self.survivor.get_body_vis_state([self.kiwiPos, self.housePos])
-        print("State: ", state_body_vis)
-        enum_state = [self.convert_to_state(state_body_vis[0]), self.convert_to_state(state_body_vis[1])]
-        print(enum_state)
-        return enum_state
-
-    def convert_to_state(self, stateNum, lenOfObj = 2):
-        if stateNum >= lenOfObj:
-            return States.NOTHING
-        elif stateNum > 1:
-            return States.SOMEONE
-        else:
-            return States(stateNum)
-    
     def do_action(self, num = -1):
         if num == -1:
             # Do random choice
             num = randint(0, 2)
-        if num == 0:
+        if num == Actions.LEFT.value:
             self.survivor.look_left()
-        elif num == 1:
-            self.survivor.look_left(True)
-        elif num == 2:
+        elif num == Actions.RIGHT.value:
+            self.survivor.look_left(False)
+        elif num == Actions.FORWARD.value:
             self.survivor.move_forward()
         else:
             print("Something is wrong when doing an action: ", num)
-
-        self.reward -= 1
-        print(self.reward)
+        self.calculate_rewards(self.get_state(self.survivor.get_body_pos(), self.survivor.get_vision_pos()))
     
-    def calculate_rewards(self, enum_state):
-        if enum_state[0].value == 1:
-            self.reward += 5
-            print("Ending reward: ", self.reward)
-            self.win.close()
-            sys.exit()
-        elif enum_state[0].value == 0:
-            print("Standing on KIWI")
-            if not self.visited_kiwi:
-                print("Kiwi visited")
-                self.reward += 30
-                self.visited_kiwi = True
-        elif enum_state[0].value == 2:
-            print("Oh, no, another person")
-            self.reward -= 10
+    def calculate_rewards(self, state):
+        print(States(state[0]), States(state[1]))
+        if state[0] == States.NOTHING.value:
+            return -1, False
+        elif state[0] == States.SOMEONE.value:
+            return -10, False
+        elif state[0] == States.KIWI.value:
+            if self.have_been_to_kiwi:
+                return 0, False
+            else:
+                self.have_been_to_kiwi = True
+                return 50, False
+        elif state[0] == States.HOUSE.value:
+            if self.have_been_to_kiwi:
+                print("Wooow! House reached after Kiwi!", self.reward + 200)
+                return 200, True
+            else:
+                print("House reached: ", self.reward + 30)
+                return 30, True
+            
+            return 10, True
+        elif state[0] == States.BORDER.value:
+            print("Out of the border.", self.reward - 1000)
+            return -1000, True
+        else:
+            print("Something is wrong: ", state)
 
-class States(enum.Enum): 
-    KIWI = 0
-    HOUSE = 1
-    SOMEONE = 2
-    NOTHING = 3
-    BORDER = 4
+
--- MovableObj.py
+++ MovableObj.py
@@ -1,41 +1,6 @@
 from graphics import *
 import math
-"""
-class MovableObjGraphic():
-    def __init__(self, window):
-        self.win = window
-        self.body = Rectangle(Point(5.3,5.3), Point(5.8,5.8)) # size 0.5
-        self.body.draw(self.win)
-        self.body.setFill("white")
-        self.vision = Rectangle(Point(5,6), Point(6,7))
-        self.vision.draw(self.win)
-        self.vision.setFill("blue1")
-        
-    def get_body_pos(self):
-        return [math.floor(self.body.getCenter().getX()), math.floor(self.body.getCenter().getY())]
-    
-    def get_vision_pos(self):
-        return [math.floor(self.vision.getCenter().getX()), math.floor(self.vision.getCenter().getY())]
-    
-    def move_in_direction(self, direction):
-        self.vision.move(direction[0], direction[1])
-        self.body.move(direction[0], direction[1])
-    
-    def look_around(self, xPos, yPos):
-        self.vision.move(xPos, yPos)
 
-    def restart(self):
-        self.vision.undraw()
-        self.vision = Rectangle(Point(5,6), Point(6,7))
-        self.vision.draw(self.win)
-        self.vision.setFill("blue1")
-        self.body.undraw()
-        self.body = Rectangle(Point(5.3,5.3), Point(5.8,5.8))
-        self.body.draw(self.win)
-        self.body.setFill("white")
- """   
-
-"""
 class MovableObj():
     up = [0, 1]
     down = [0, -1]
@@ -56,13 +21,11 @@ class MovableObj():
         return (self.current_pos[0] + self.states[self.currentState][0], self.current_pos[1] + self.states[self.currentState][1]) 
     
     def do_action(self, action_num):
-        if action_num == 0: # look left
-            self.look_left()
-        elif action_num == 1: # look right
-            self.look_left(False)
-        else:
+        if action_num == 2:
             self.move_forward()
-
+        else:
+            self.look_left(action_num)
+            
     def move_forward(self):
         direction = self.states[self.currentState]
         self.current_pos = (self.current_pos[0] + direction[0], self.current_pos[1] + direction[1]) 
@@ -70,23 +33,9 @@ class MovableObj():
     def restart(self):
         self.currentState = 0
 
-    def look_left(self, left = True):
-        nextState = self.directions[self.currentState][0 if left else 1]
-        
-        #print("\nCurrentState: ", self.currentState, "left: ", left)
-        #print("nextState: ", nextState)
-
-        xPos = self.moves[self.currentState][0 if left else 1][0] #xDestination - visionPos[0]
-        yPos = self.moves[self.currentState][0 if left else 1][1] #yDestination - visionPos[1]
-        #print(xPos, yPos)
-        #print("newPos", xPos, yPos)
-
+    def look_left(self, left = 0):
+        nextState = self.directions[self.currentState][left]
         self.currentState = nextState
-        #print(self.vision.getP1(), self.vision.getP1())
-        #print("newState: ", self.currentState)
-
-"""
-# Working copy:
 
 class GraphicMovableObj():
     up = [0, 1]
@@ -132,7 +81,9 @@ class GraphicMovableObj():
     # TODO FIX not ending when found
     def get_body_vis_state(self, object_positions):
         visionPos = self.get_vision_pos()
+        
         bodyPos = self.get_body_pos()
+        print(visionPos, bodyPos)
         state_body_vis = [len(object_positions) + 1, len(object_positions) + 1]
         for i in range(len(object_positions)):
             if visionPos[0] == object_positions[i][0] and visionPos[1] == object_positions[i][1]:
